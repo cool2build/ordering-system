09dcae1ef5dfcbffcb23a0e5f549c3a2d9b7b9e2
Kenly Hui <kenlyhui@Kenlys-MacBook-Pro.local>
2017-08-29 10:34:25 -0700

    edited

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.use(express.static('./'));
-// app.use(express.static('dist'));
+app.use(express.static('./'));
+app.use(express.static('dist'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(`${__dirname}/dist/index.html`);
-// });
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-// app.listen(port, () => {
-//   console.log('app listening on', port);
-// });
+app.listen(port, () => {
+  console.log('app listening on', port);
+});
